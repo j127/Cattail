5d2b267c8df6ad74eda00c8001f09cd84b37dcf1
J. Cohen <hostelmarketing@gmail.com>
2013-11-18 21:10:08 -0800

    First version of script

---
+++ sz.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Please note, this is a work in progress just in the early stages
+# Don't use this script unless you understand exactly what it does!
+# It doesn't check for existing files and could delete something important.
+
+helpme () {
+echo "sz: a toolbox for automating repetitive stuff in the terminal.";
+echo "sz init - starts a git repo in the current directory with a .gitignore. Does the initial commit."
+}
+
+if [ $1 ]
+then
+    case "$1" in
+        init)
+            echo "Creating git repo"
+            git init
+            echo "Adding .gitignore"
+            echo ".*~\n*.swp\n*.swo\n*.pyc" >> .gitignore
+            echo "Adding .gitignore to repo"
+            git add .gitignore
+            echo "Committing to repo"
+            git commit -m "Initial commit by sz"
+            echo "Done"
+            ;;
+        commit)
+            echo "This doesn't do anything yet."
+            ;;
+        *)
+            helpme;
+            ;;
+    esac
+else
+    helpme
+fi
+
+
