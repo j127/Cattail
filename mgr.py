4524f588b3b19f209b7536c3ac11e3633d91d010
J. Cohen <hostelmarketing@gmail.com>
2015-04-20 00:59:12 -0700

    Add docstrings

--- mgr.py
+++ mgr.py
@@ -11,9 +11,11 @@ parser = argparse.ArgumentParser(
 args = parser.parse_args()
 
 def make_git_repo():
+    """Makes a Git repo with the default .gitignore."""
     return 'hi there'
 
 def main():
+    """Checks if the script is being run from within an existing Git repo."""
     if call(["git", "branch"], stderr=STDOUT, stdout=open(os.devnull, 'w')) != 0:
         make_git_repo()
     else:
